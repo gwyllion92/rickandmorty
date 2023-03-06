312ccbdf8e3021c75432ef4797cd87a4623f9509
gwyllion92 <glesta92@protonmail.com>
2023-03-06 09:49:00 -0300

    version 1.5

---
+++ src/redux/reducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+    myFavorites: []
+}
+
+const reducer = (state = initialState, { type, payload }) => {
+    switch(type){
+
+
+        default:
+            return {...state}
+    }
+}
+
+export default reducer
