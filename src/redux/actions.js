65c845e39c043244a0c1a020ea7142f0fe905933
gwyllion92 <glesta92@protonmail.com>
2023-03-06 09:56:23 -0300

    version 1.5.1

---
+++ src/redux/actions.js
@@ -0,0 +1,15 @@
+import { ADD_FAVORITE, DELETE_FAVORITE  } from './action-types'
+
+export const addFavorite = (character) => {
+    return{
+        type: ADD_FAVORITE,
+        payload: character
+    }
+}
+
+export const deleteFavorite = (id) => {
+    return{
+        type: DELETE_FAVORITE,
+        payload: id
+    }
+}
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,10 +1,22 @@
+import { ADD_FAVORITE, DELETE_FAVORITE  } from './action-types'
+
 const initialState = {
     myFavorites: []
 }
 
 const reducer = (state = initialState, { type, payload }) => {
     switch(type){
+        case ADD_FAVORITE:
+            return{
+                ...state,
+                myFavorites: [...state.myFavorites, payload]
+            }
 
+        case DELETE_FAVORITE :
+            return{
+                ...state,
+                myFavorites: state.myFavorites.filter(character => character.id !== payload)
+            }
 
         default:
             return {...state}
